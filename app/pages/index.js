a80160d61e2e3c1163413cd19f7961c7f8a22515
Andika Nugraha <m.andika.nugraha@gmail.com>
2021-02-01 21:43:18 +0700

    Title

--- app/pages/index.js
+++ app/pages/index.js
@@ -37,7 +37,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Crypto Lucu-Lucuan</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
